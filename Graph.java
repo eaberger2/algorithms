4f8d32cb7cbfe9df01192b2246d8cfc663f8615b
Emily Berger <eberger@snares-07.cs.wisc.edu>
2022-10-20 15:31:31 -0500

    Beginnings of ShortestPath

--- Graph.java
+++ Graph.java
@@ -1,46 +1,13 @@
 import java.util.ArrayList;
-
-public class Node{
-
-    public int data;
-    public ArrayList<Edge> edges;
-
-    public Node(int data){
-        this.data = data;
-    }
-
-    public void addEdge(Node t,int length){
-        this.edges.add(new Edge(t,length))
-    }
-
-}
-
-public class Edge{
-    
-    public int length;
-    public Node end;
-
-    public Edge(Node e,int l){
-        this.end = e;
-        this.length = l;
-    }
-
-    public Node getEnd(){
-        return this.end;
-    }
-
-    public int getLength(){
-        return this.length;
-    }
-
-}
+import Node.java;
+import Edge.java;
 
 public class Graph {
 
     public ArrayList<Node> nodes;
 
     public Graph(){
-        nodes = new ArrayList<Node>();
+        this.nodes = new ArrayList<Node>();
     }
 
     public void addNode(Node n){
